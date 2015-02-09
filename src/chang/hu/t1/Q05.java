3d6d2ef8d6ff3ddecf39d69ae85b072ab8467adb
changhu <o52tiger@gmail.com>
2015-02-09 11:47:25 +0800

    --

--- src/chang/hu/t1/Q05.java
+++ src/chang/hu/t1/Q05.java
@@ -2,6 +2,10 @@ package chang.hu.t1;
 
 public class Q05 {
 
+	/**
+	 * ¥Ú”°æ≈æ≈≥À∑®±Ì
+	 * @param args
+	 */
 	public static void main(String[] args){
 		for(int i = 1; i < 10; i++){
 			for(int j = 1; j < 10; j++){
